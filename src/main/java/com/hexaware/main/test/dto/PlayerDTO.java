5bbf942fcf4d9dadaf6782c8e33c613bf9bf15fe
Team15hexa <cocmobile2005@gmail.com>
2026-06-24 12:39:01 +0530

    commit1

--- src/main/java/com/hexaware/main/test/dto/PlayerDTO.java
+++ src/main/java/com/hexaware/main/test/dto/PlayerDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +31,10 @@ public class PlayerDTO {
 	
 	@NotNull
 	@NotBlank
+	@Pattern(
+    regexp = "Batsman|Bowler|Keeper|All Rounder",
+    message = "Role must be Batsman, Bowler, Keeper or All Rounder"
+)
 	private String role;
 	
 	@NotNull
@@ -42,7 +47,7 @@ public class PlayerDTO {
 	
 	@NotNull
 	@NotBlank
-	private String countryStateName;
+	private String StateName;
 	
 	@NotBlank
 	private String description;
